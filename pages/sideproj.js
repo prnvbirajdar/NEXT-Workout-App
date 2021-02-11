ba501c8a961cb52370a610182d719e84a068dab9
prnvbirajdar <prnvbirajdar@gmail.com>
2021-02-11 05:34:34 -0500

    side proj working

---
+++ pages/sideproj.js
@@ -0,0 +1,46 @@
+import Navbar from "../components/Navbar";
+import { useAuth } from "../components/data/authProvider";
+import { db } from "../components/data/firebase";
+
+const SideProg = () => {
+  const [text, setText] = React.useState("");
+
+  const { user, loading } = useAuth(); //context
+
+  console.log(user.uid);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("random")
+      .doc(user.uid)
+      .set({
+        message: { text },
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+  };
+
+  return (
+    <div className=" text-gray-600 dark:text-gray-400">
+      <Navbar />
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-content items-center flex-col"
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default SideProg;
