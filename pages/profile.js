16539de38fe4819a02081a34bcef9c6dc66f0cd0
prnvbirajdar <prnvbirajdar@gmail.com>
2021-02-11 18:34:13 -0500

    data read working

--- pages/profile.js
+++ pages/profile.js
@@ -17,34 +17,37 @@ const Profile = () => {
   const { register, handleSubmit, errors } = useForm();
   const { user } = useAuth(); //context
 
-  // export const getPhysicalStats = async (id) => {
-  //   const snapshot = await db.collection(id).get();
-  //   const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  //   console.log(data);
-  // };
+  //   const BMIdata = (w, h) => {
+  //     if (w > 0 && h > 0) {
+  //       return (w * 703) / h / h;
+  //     }
+  //   };
+
+  //console.log(user?.uid);
+
+  //   const getPhysicalStats = async () => {
+  //     await db
+  //       .collection("profiles")
+  //       .onSnapshot((querySnapshot) => {
+  //         console.log(querySnapshot.docs.map(doc=>console.log(doc.data())));
+  //         setPhysicalStats(querySnapshot.docs);
+  //       });
+  //   };
 
   console.log(user?.uid);
 
   const getPhysicalStats = async () => {
     await db
       .collection("profiles")
+      .doc(user?.uid)
+      .collection("stats")
+      .orderBy("timeStamp", "desc")
+      .limit(1)
       .onSnapshot((querySnapshot) => {
-        console.log(querySnapshot.docs.map(doc=>console.log(doc.data())));
-        setPhysicalStats(querySnapshot.docs);
+        console.log(querySnapshot.docs.map((d) => console.log(d.data())));
+        setPhysicalStats(querySnapshot.docs[0].data());
       });
   };
-
-  //   const getPhysicalStats = async () => {
-  //     await db
-  //       .collection("profiles")
-  //       .doc(user?.uid)
-  //       .collection("stats")
-  //       .orderBy("timeStamp", "asc")
-  //       .onSnapshot((querySnapshot) => {
-  //         console.log(querySnapshot.docs.slice(-1));
-  //         setPhysicalStats(querySnapshot.docs.slice(-1));
-  //       });
-  //   };
 
   function openProfileModal() {
     setIsProfileModalOpen(true);
@@ -57,16 +60,18 @@ const Profile = () => {
     if (user) {
       getPhysicalStats();
     }
-  }, []);
+  }, [user]);
 
   const onSubmit = (data) => {
     //setPhysicalStats(data);
     db.collection("profiles")
+      .doc(user.uid)
+      .collection("stats")
       .add({
-        Weight: data.weight,
-        Height: data.height,
-        "Daily Calories": data.dailyCalories,
-        "Body Fat Percentage": data.bodyFatPercentage,
+        weight: data.weight,
+        height: data.height,
+        dailyCalories: data.dailyCalories,
+        bodyFatPercentage: data.bodyFatPercentage,
         timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
         profileId: user.uid,
       })
@@ -91,20 +96,37 @@ const Profile = () => {
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col justify-around p-2 m-4">
                   <label>Weight</label>
+                  <p>{physicalStats.weight ? physicalStats.weight : "0"} lbs</p>
                   <input type="number" name="weight" ref={register} />
+                  <p>
+                    {physicalStats.height ? physicalStats.height : "0"} inches
+                  </p>
                   <label>Height</label>
+
                   <input type="number" name="height" ref={register} />
                   <label>Body Fat Percentage</label>
+                  <p>
+                    {physicalStats.bodyFatPercentage
+                      ? physicalStats.bodyFatPercentage
+                      : "0"}{" "}
+                    %
+                  </p>
+
                   <input
                     type="number"
                     name="bodyFatPercentage"
                     ref={register}
                   />
                   <label>Daily Calories</label>
+                  <p>
+                    {physicalStats.dailyCalories
+                      ? physicalStats.dailyCalories
+                      : "0"}{" "}
+                    calories
+                  </p>
                   <input type="number" name="dailyCalories" ref={register} />
-                  {/*<label>BMI</label>
-                <input type="number"  name="weight"
-                ref={register} />*/}
+                  {/* <label>BMI</label>
+                 <p>{BMIdata(physicalStats.height, physicalStats.weight)}</p>*/}
                 </div>
                 <div className="flex justify-end">
                   <Button type="submit">Save</Button>
--- pages/sideproj.js
+++ pages/sideproj.js
@@ -5,18 +5,17 @@ import { db } from "../components/data/firebase";
 const SideProg = () => {
   const [text, setText] = React.useState("");
 
-  const { user} = useAuth(); //context
+  const { user, loading } = useAuth(); //context
 
-  //const [textArray, setTextArray] = React.useState([]);
+  console.log(user.uid);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    db.collection("hello")
-      .doc(user.uid)
-      .collection("stats")
-      .add({
-        message: { text },
+    db.collection("cities")
+      .doc("LA")
+      .set({
+        name: text,
       })
       .then(() => {
         console.log("Document successfully written!");
@@ -45,3 +44,48 @@ const SideProg = () => {
 };
 
 export default SideProg;
+
+// import Navbar from "../components/Navbar";
+// import { useAuth } from "../components/data/authProvider";
+// import { db } from "../components/data/firebase";
+
+// const SideProg = () => {
+//   const [text, setText] = React.useState("");
+
+//   const { user} = useAuth(); //context
+
+//   //const [textArray, setTextArray] = React.useState([]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     db.collection("hello").push({
+//         message: { text },
+//       })
+//       .then(() => {
+//         console.log("Document successfully written!");
+//       })
+//       .catch((error) => {
+//         console.error("Error writing document: ", error);
+//       });
+//   };
+
+//   return (
+//     <div className=" text-gray-600 dark:text-gray-400">
+//       <Navbar />
+//       <form
+//         onSubmit={handleSubmit}
+//         className="flex justify-content items-center flex-col"
+//       >
+//         <input
+//           type="text"
+//           value={text}
+//           onChange={(e) => setText(e.target.value)}
+//         />
+//         <button type="submit">Send</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default SideProg;
