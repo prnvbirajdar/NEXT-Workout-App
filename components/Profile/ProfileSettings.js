7f38293dc9b66352607824b957faa958d57b182c
prnvbirajdar <prnvbirajdar@gmail.com>
2021-02-10 22:48:57 -0500

    react hook form

--- components/Profile/ProfileSettings.js
+++ components/Profile/ProfileSettings.js
@@ -1,47 +1,225 @@
 import { useState } from "react";
-import { Card, CardBody } from "@windmill/react-ui";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@windmill/react-ui";
+import { useForm } from "react-hook-form";
 
-const ProfileSettings = () => {
-  const [value, setvalue] = useState({
-    weight: "",
-    height: "",
-    bodyFatPercentage: "",
-    dailyCalories: "",
-    BMI: "",
-  });
+const ProfileSettings = ({ closeProfileModal, isProfileModalOpen }) => {
+  const [physicalStats, setPhysicalStats] = useState(null);
+
+  //   const [value, setValue] = useState({
+  //     weight: "",
+  //     height: "",
+  //     bodyFatPercentage: "",
+  //     dailyCalories: "",
+  //     BMI: "",
+  //   });
+
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => setPhysicalStats(data);
+  console.log(errors);
+
+  //   const handleValueChange = (e) => {
+  //     const { name, value } = e.target;
+  //     setValue((prevState) => ({ ...prevState, [name]: value }));
+  //   };
+
+  //   const handleValueSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log(value);
+  //   };
+
+  //   console.log(value);
+
+  //   <form onSubmit={handleSubmit(onSubmit)}>
+  //       <input type="number" placeholder="Weight" name="Weight" ref={register({pattern: /^\S+@\S+$/i})} />
+  //       <input type="number" placeholder="Height" name="Height" ref={register({pattern: /^\S+@\S+$/i})} />
+  //       <input type="number" placeholder="Daily Calories" name="Daily Calories" ref={register({pattern: /^\S+@\S+$/i})} />
+  //       <input type="number" placeholder="Body Fat Percentage" name="Body Fat Percentage" ref={register} />
+
+  //       <input type="submit" />
+  //     </form>
 
   return (
-    <div className="w-1/2 shadow text-gray-600 dark:text-gray-400 ">
-      <Card>
-        <CardBody>
-          <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300 text-center text-xl">
-            Profile Settings
-          </p>
-          <div className="flex flex-col md:flex-row justify-around p-2 m-4">
-            <label>Weight</label>
-            <label>Height</label>
-            <label>Body Fat Percentage</label>
-            <label>Daily Calories</label>
-            <label>BMI</label>
+    <Modal isOpen={isProfileModalOpen} onClose={closeProfileModal}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalHeader>Physical Stats</ModalHeader>
+        <ModalBody>
+          <div className="flex flex-col ">
+            <div className="text-center text-base pb-2">
+              <label>Weight</label>
+            </div>
+            <div className="flex justify-center ">
+              <input
+                className="py-2 rounded w-1/3 border text-black text-center	"
+                type="number"
+                placeholder="Weight"
+                name="weight"
+                ref={register}
+              />
+            </div>
+            <div className="text-center text-base p-2">
+              <label>Height</label>
+            </div>
+            <div className="flex justify-center ">
+              <input
+                className="py-2 rounded w-1/3 border text-black text-center	"
+                placeholder="Height"
+                name="height"
+                ref={register}
+              />
+            </div>
+            <div className="text-center text-base p-2">
+              <label>Daily Calories</label>
+            </div>
+            <div className="flex justify-center ">
+              <input
+                className="py-2 rounded w-1/3 border text-black text-center	"
+                placeholder="Daily Calories"
+                name="dailyCalories"
+                ref={register}
+              />
+            </div>
+            <div className="text-center text-base p-2">
+              <label>Body Fat Percentage</label>
+            </div>
+            <div className="flex justify-center ">
+              <input
+                className="py-2 rounded w-1/3 border text-black text-center	"
+                ref={register}
+                type="number"
+                name="bodyFatPercentage"
+                placeholder="Body Fat Percentage"
+              />
+            </div>
+            {/*  <div className="text-center text-base p-2">
+              <label>BMI</label>
+            </div>
+            <div className="flex justify-center ">
+              <input
+                className="py-2 rounded w-1/3 border text-black text-center	"
+                onChange={handleValueChange}
+                type="number"
+                name="BMI"
+              />
+  </div>*/}
           </div>
-        </CardBody>
-      </Card>
-    </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            className="w-full sm:w-auto"
+            layout="outline"
+            onClick={closeProfileModal}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" className="w-full sm:w-auto">
+            Accept
+          </Button>
+        </ModalFooter>
+      </form>
+    </Modal>
   );
 };
 
 export default ProfileSettings;
 
-// physicalStats
-// BMI
-// 0
-// bodyFatPercentage
-// 20.5
-// dailyCalories
-// 2500
-// height
-// 72
-// weight
-// 200
-// picture
-// ""
+// <Modal isOpen={isProfileModalOpen} onClose={closeProfileModal}>
+// <ModalHeader>Physical Stats</ModalHeader>
+// <ModalBody>
+//   <div className="flex flex-col ">
+//     <div className="text-center text-base pb-2">
+//       <label>Weight</label>
+//     </div>
+//     <div className="flex justify-center ">
+//       <input
+//         className="py-2 rounded w-1/3 border text-black text-center	"
+//         onChange={handleValueChange}
+//         type="number"
+//         name="weight"
+//       />
+//     </div>
+//     <div className="text-center text-base p-2">
+//       <label>Height</label>
+//     </div>
+//     <div className="flex justify-center ">
+//       <input
+//         className="py-2 rounded w-1/3 border text-black text-center	"
+//         onChange={handleValueChange}
+//         type="number"
+//         name="height"
+//       />
+//     </div>
+//     <div className="text-center text-base p-2">
+//       <label>Daily Calories</label>
+//     </div>
+//     <div className="flex justify-center ">
+//       <input
+//         className="py-2 rounded w-1/3 border text-black text-center	"
+//         onChange={handleValueChange}
+//         type="number"
+//         name="dailyCalories"
+//       />
+//     </div>
+//     <div className="text-center text-base p-2">
+//       <label>Body Fat Percentage</label>
+//     </div>
+//     <div className="flex justify-center ">
+//       <input
+//         className="py-2 rounded w-1/3 border text-black text-center	"
+//         onChange={handleValueChange}
+//         type="number"
+//         name="bodyFatPercentage"
+//       />
+//     </div>
+//     <div className="text-center text-base p-2">
+//       <label>BMI</label>
+//     </div>
+//     <div className="flex justify-center ">
+//       <input
+//         className="py-2 rounded w-1/3 border text-black text-center	"
+//         onChange={handleValueChange}
+//         type="number"
+//         name="BMI"
+//       />
+//     </div>
+//   </div>
+// </ModalBody>
+// <ModalFooter>
+//   <Button
+//     className="w-full sm:w-auto"
+//     layout="outline"
+//     onClick={closeProfileModal}
+//   >
+//     Cancel
+//   </Button>
+//   <div >
+//     <Button onClick={handleValueSubmit} className="w-full sm:w-auto">Accept</Button>
+//   </div>
+// </ModalFooter>
+// </Modal>
+
+// <div className="w-1/2 shadow text-gray-600 dark:text-gray-400 ">
+// <Card>
+//   <CardBody>
+//     <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300 text-center text-xl">
+//       Profile Settings
+//     </p>
+//     <div className="flex flex-col md:flex-row justify-around p-2 m-4">
+//       <label>Weight</label>
+//       <label>Height</label>
+//       <label>Body Fat Percentage</label>
+//       <label>Daily Calories</label>
+//       <label>BMI</label>
+//     </div>
+//     <div className="flex justify-end">
+//       <Button>Edit</Button>
+//     </div>
+//   </CardBody>
+// </Card>
+// </div>
--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import ProfileSettings from "../components/Profile/ProfileSettings";
+import { Card, CardBody, Button } from "@windmill/react-ui";
 
 const Profile = () => {
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+
+  function openProfileModal() {
+    setIsProfileModalOpen(true);
+  }
+  function closeProfileModal() {
+    setIsProfileModalOpen(false);
+  }
+
   return (
     <div>
       <Navbar />
       <div className="flex justify-center mt-12 pt-5">
-        <ProfileSettings />
+        <div className="w-1/2 shadow text-gray-600 dark:text-gray-400 ">
+          <Card>
+            <CardBody>
+              <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300 text-center text-xl">
+                Profile Settings
+              </p>
+              <div className="flex flex-col md:flex-row justify-around p-2 m-4">
+                <label>Weight</label>
+                <label>Height</label>
+                <label>Body Fat Percentage</label>
+                <label>Daily Calories</label>
+                <label>BMI</label>
+              </div>
+              <div onClick={openProfileModal} className="flex justify-end">
+                <Button>Edit</Button>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
       </div>
+      <ProfileSettings isProfileModalOpen={isProfileModalOpen} closeProfileModal={closeProfileModal}/>
     </div>
   );
 };
