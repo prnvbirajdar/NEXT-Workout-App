fc0bee0ab7c108f0c624608256299e2f7d1a736b
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-24 05:38:46 -0400

    conditional removing personal details if anon singin

--- components/Profile/PersonalDetailsCard.js
+++ components/Profile/PersonalDetailsCard.js
@@ -1,30 +1,33 @@
 import { Card, CardBody } from "@windmill/react-ui";
 
 const PersonalDetailsCard = ({ user }) => {
+  console.log(user);
   return (
-    <div className="w-full">
-      <Card>
-        <CardBody>
-          <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300 text-center text-xl">
-            Personal Information
-          </p>
-          <div className="flex flex-col justify-around p-2 mx-4 mb-2">
-            <div className="flex justify-between p-2 transition bg-gray-50 dark:bg-black rounded mb-3">
-              <label>Username</label>
-              <p className=" text-gray-800 font-medium ml-3 capitalize text-right dark:text-gray-100">
-                {user?.displayName ? user?.displayName : ""}
-              </p>
+    user?.displayName === null || user?.email === null && (
+      <div className="w-full">
+        <Card>
+          <CardBody>
+            <p className="mb-4 font-semibold text-gray-600 dark:text-gray-300 text-center text-xl">
+              Personal Information
+            </p>
+            <div className="flex flex-col justify-around p-2 mx-4 mb-2">
+              <div className="flex justify-between p-2 transition bg-gray-50 dark:bg-black rounded mb-3">
+                <label>Username</label>
+                <p className=" text-gray-800 font-medium ml-3 capitalize text-right dark:text-gray-100">
+                  {user?.displayName ? user?.displayName : ""}
+                </p>
+              </div>
+              <div className="flex justify-between p-2 transition bg-gray-50 dark:bg-black rounded mb-3">
+                <label>Email</label>
+                <p className=" text-gray-800 font-medium ml-3 break-all	 text-right dark:text-gray-100">
+                  {user?.email ? user?.email : ""}
+                </p>
+              </div>
             </div>
-            <div className="flex justify-between p-2 transition bg-gray-50 dark:bg-black rounded mb-3">
-              <label>Email</label>
-              <p className=" text-gray-800 font-medium ml-3 break-all	 text-right dark:text-gray-100">
-                {user?.email ? user?.email : ""}
-              </p>
-            </div>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+          </CardBody>
+        </Card>
+      </div>
+    )
   );
 };
 
