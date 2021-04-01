55c8083ad2dc7f1864a82bbb85a16679169c73b5
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-01 17:47:11 -0400

    continue as guest semibold

--- components/Login.js
+++ components/Login.js
@@ -75,7 +75,7 @@ const Login = () => {
                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                       />
                     </svg>
-                    <p className="text-sm font-semibold"> Continue as Guest</p>
+                    <p className="text-sm font-semibold">Continue as Guest</p>
                   </button>
                   <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
                 </div>
