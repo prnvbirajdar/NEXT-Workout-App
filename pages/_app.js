ebaeb36ca4dfbe7a168f9d14f64ab82952afd2e8
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-30 02:20:08 -0400

    added linkedin image

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,16 @@ function MyApp({ Component, pageProps }) {
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" />
       <Head>
         <title>Lift Workout App</title>
+        <meta name="Description" content="This is a home page of my website!" />
+        <meta name="title" property="og:title" content="Lift Workout App" />
+        <meta property="og:type" content="Website" />
+        <meta name="image" property="og:image" content="https://live.staticflickr.com/65535/51211255247_6d6db32267_k.jpg" />
+        <meta name="description" property="og:description" content="Lift Workout App" />
+        <meta name="author" content="Pranav Birajdar" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="Lift Icon" type="image/ico" href="/dumbell.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="Description" content="This is a home page of my website!" />
       </Head>
       <AuthProvider>
         <Windmill usePreferences theme={myTheme}>
