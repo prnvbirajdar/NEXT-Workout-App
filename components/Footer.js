f0f5af13ad48c851235e01172f3ebe999a69802e
Pranav Birajdar <prnvbirajdar@gmail.com>
2022-01-03 14:37:19 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="absolute bottom-0 w-full h-8">
       <p className=" text-center text-sm font-medium text-gray-800 hover:text-indigo-900 dark:text-gray-400 dark:hover:text-indigo-400">
         <a href="https://github.com/prnvbirajdar">
-          ©2021 Made with ❤️ by Pranav Birajdar
+          ©{new Date().getFullYear()} Made with ❤️ by Pranav Birajdar
         </a>
       </p>
     </footer>
