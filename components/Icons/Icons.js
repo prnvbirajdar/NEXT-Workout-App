732bef75bd55c19b527c041320ff02ecb812f686
prnvbirajdar <prnvbirajdar@gmail.com>
2023-01-05 01:23:38 -0500

    icons edit

--- components/Icons/Icons.js
+++ components/Icons/Icons.js
@@ -35,9 +35,10 @@ const Paste = ({ lineHeight, lineWidth, circleHeight, circleWidth }) => {
   );
 };
 
-const Correct = () => {
+const Correct = ({ onClick }) => {
   return (
     <button
+      onClick={onClick}
       aria-label="correct"
       className=" inline-flex items-center justify-center w-10 h-10 text-white transition-colors bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
     >
@@ -100,6 +101,16 @@ const Home = () => {
           strokeWidth={2}
           d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
         />
+      </svg>
+    </button>
+  );
+};
+
+const Calculator = () => {
+  return (
+    <button>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z" />
       </svg>
     </button>
   );
@@ -324,4 +335,5 @@ export {
   LiftLogo,
   CopySet,
   Paste,
+  Calculator
 };
